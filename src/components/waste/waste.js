d23840ab03f3cde1ba8af5d52a9fad0869c893df
Divya Krishnan <divya.brookfield@gmail.com>
2022-06-23 16:34:24 -0500

    asdaassdfsdf

--- src/components/waste/waste.js
+++ src/components/waste/waste.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ItemList from './ItemList';
 import ItemInfoModal from './ItemInfoModal';
 import { getWasteList, addWaste, updateWaste } from '../../services/services';
-import { Page, Navbar, List, ListButton } from 'framework7-react';
 
 const cities = ['Milwaukee', 'Phoenix', 'San Fransisco', 'Dallas'];
 function Waste(props) {
@@ -14,29 +13,19 @@ function Waste(props) {
       }}
     ></a>;
   });
+
+  return (
+    <VersionPageHolder>
+      {
+        <Waste
+          onChange={(city) => {
+            setSelectedCity(city);
+          }}
+        />
+      }
+      <div>{renderElements(selectedCity)}</div>
+    </VersionPageHolder>
+  );
 }
 
-return (
-  <VersionPageHolder>
-    {
-      <Waste
-        onChange={(city) => {
-          setSelectedCity(city);
-        }}
-      />
-    }
-    <div>{renderElements(selectedCity)}</div>
-  </VersionPageHolder>
-);
-
-// export default () => (
-//   <Page>
-//     <Navbar title="List Button" />
-
-//     <List inset>
-//       <ListButton title="List Button 1" />
-//       <ListButton title="List Button 2" />
-//       <ListButton title="List Button 3" />
-//     </List>
-//   </Page>
-//);
+export default Waste;
