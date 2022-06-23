f3bf27a124c3467c5d3f339406b87de16c91990f
SeanR <s.m.reagan22@gmail.com>
2022-06-23 09:39:07 -0500

    itemList

--- src/components/waste/ItemList.js
+++ src/components/waste/ItemList.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function ItemList(props) {
   const activeUsers = props.items.filter((item) => item.active === true);
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
   if (activeUsers.length === 0) {
     return <h2>Found No Items.</h2>;
   }
