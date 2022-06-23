d68d116417872390229ba15067f3024e85433c49
Divya Krishnan <divya.brookfield@gmail.com>
2022-06-23 14:52:17 -0500

    change3

--- src/components/waste/waste.js
+++ src/components/waste/waste.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ItemList from './ItemList';
 import ItemInfoModal from './ItemInfoModal';
 import { getWasteList, addWaste, updateWaste } from '../../services/services';
-import * as React from 'react';
 
-function Waste() {
-  return (
-    <div>
-      <select>
-        <option>C1</option>
-        <option>C2</option>
-        <option>C3</option>
-      </select>
-    </div>
-  );
-}
+const numbers = [1, 2, 3, 4, 5];
+
+const updatedNums = numbers.map((number) => {
+  return <li>{number}</li>;
+});
+
+ReactDOM.render(<ul>{updatedNums}</ul>, document.getElementById('root'));
 
 export default Waste;
