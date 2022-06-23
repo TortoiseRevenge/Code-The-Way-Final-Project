b0d9c942ed818531c88452551fef66ba285501cf
Adam Miller <theadammiller56@gmail.com>
2022-06-23 12:13:15 -0500

    API calls included

--- src/services/services.js
+++ src/services/services.js
@@ -1,51 +1,58 @@
 import { parseFetchResponse } from '../parse-fetch-response/parse-fetch-response';
 
-const baseURL = 'https://dev-lead2change-ctw.azurewebsites.net/example/';
+const baseWasteURL = ' https://ctw-dev-mc-wm.azurewebsites.net/';
 
 /**
- * It makes a request to the server, parses the response, and returns the students
- * @returns An array of students.
+ * It makes a request to the server, parses the response, and returns the Waste Items
+ * @returns An array of Waste Items.
  */
-export const getStudents = async () => {
-  const response = await fetch(`${baseURL}getstudents`, { method: 'GET' });
-
+export const getWasteList = async () => {
+  const response = await fetch(`${baseWasteURL}inventory`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json, text/plain',
+      'Content-Type': 'application/json',
+    },
+  });
   const parsedResponse = await parseFetchResponse(response);
+
   return parsedResponse;
 };
 
 /**
- * It makes a POST request to the endpoint, and returns the student object that was created
- * @param student - The student to add
- * @returns The student object
+ * It makes a POST request to the endpoint, and returns the Waste Item object that was created
+ * @param waste - The Waste Item to add
+ * @returns The Waste Item object
  */
-export const addStudent = async (student) => {
-  const response = await fetch(`${baseURL}createstudent`, {
+export const addWaste = async (waste) => {
+  const response = await fetch(`${baseWasteURL}inventory`, {
     method: 'POST',
     headers: {
       Accept: 'application/json, text/plain',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(student),
+    body: JSON.stringify(waste),
   });
   const parsedResponse = await parseFetchResponse(response);
   return parsedResponse;
 };
 
 /**
- * It makes a POST request to the endpoint with the updated student
- * @param student - The student to update
+ * It makes a POST request to the endpoint with the updated waste item
+ * @param waste - The waste item to update
  * @returns The response from the server.
  */
-export const updateStudent = async (student) => {
-  const response = await fetch(`${baseURL}updatestudent/${student.id}`, {
+export const updateWaste = async (waste) => {
+  const response = await fetch(`${baseWasteURL}inventory`, {
     method: 'PUT',
     headers: {
       Accept: 'application/json, text/plain',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(student),
+    body: JSON.stringify(waste),
   });
 
   const parsedResponse = await parseFetchResponse(response);
+
   return parsedResponse;
 };
