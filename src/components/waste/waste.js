0f5ff8aa031fc0153aebd569b944aca6d7b75b72
Divya Krishnan <divya.brookfield@gmail.com>
2022-06-23 11:00:23 -0500

    change2

---
+++ src/components/waste/waste.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function Waste() {
+  return (
+    <div>
+      <select>
+        <option>C1</option>
+        <option>C2</option>
+        <option>C3</option>
+      </select>
+    </div>
+  );
+}
+
+export default Waste;
