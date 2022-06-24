733aa63418c52f306716379de85d12fa885a0521
Adam Miller <theadammiller56@gmail.com>
2022-06-24 00:17:19 -0500

    Accord

--- src/components/waste/ItemList.js
+++ src/components/waste/ItemList.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Button from '@mui/material/Button';
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -10,21 +11,21 @@ import Grid from '@mui/material/Grid';
 import ItemInfoModal from './TestModal';
 
 function ItemList(props) {
-  const activeUsers = props.items.filter((item) => item.active === true);
   const [expanded, setExpanded] = React.useState(false);
 
   const handleChange = (panel) => (event, isExpanded) => {
-    <ItemInfoModal items={activeUsers} />;
+    <ItemInfoModal items={props.items} />;
     setExpanded(isExpanded ? panel : false);
   };
-  if (activeUsers.length === 0) {
-    return <h2>Found No Items.</h2>;
-  }
+
+  const editHandler = (event) => {
+    // TODO
+  };
   return (
     <Grid container spacing={2} justifyContent="center" rowspacing={2}>
       <Grid item xs={5} spacing={1}>
         <div>
-          {activeUsers.map((itemObject, index) => (
+          {props.items.map((itemObject, index) => (
             <Accordion
               expanded={expanded === 'panel'.concat(index.toString())}
               onChange={handleChange('panel'.concat(index.toString()))}
@@ -35,17 +36,26 @@ function ItemList(props) {
                 id="panel1bh-header"
               >
                 <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                  {itemObject.itemName}
+                  {itemObject.name}
                 </Typography>
                 <Typography sx={{ color: 'text.secondary' }}>
                   {itemObject.owner}
                 </Typography>
               </AccordionSummary>
               <AccordionDetails>
+                <Typography>Price: ${itemObject.price}</Typography>
+                <Typography>City: {itemObject.city}</Typography>
+                <Typography>State: {itemObject.state}</Typography>
+                <Typography>Postal Code: {itemObject.postalCode}</Typography>
                 <Typography>
-                  Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
-                  feugiat. Aliquam eget maximus est, id dignissim quam.
+                  Date Accepted: {itemObject.dateAccepted}
                 </Typography>
+                <Typography>
+                  Date Returned: {itemObject.dateReturned}
+                </Typography>
+                <Button value={itemObject} onClick={editHandler}>
+                  Edit This Item
+                </Button>
               </AccordionDetails>
             </Accordion>
           ))}
--- src/components/waste/waste.js
+++ src/components/waste/waste.js
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import { useNavigate } from 'react-router-dom';
 import Info from './Info';
 import ItemList from './ItemList';
 import ItemInfoModal from './TestModal';
 import AddItemModal from './addItemModal';
 import { getWasteList, addWaste, updateWaste } from '../../services/services';
 
-const cities = ['Milwaukee', 'Phoenix', 'San Fransisco', 'Dallas'];
 function Waste(props) {
-  return cities.map((city) => {
-    <a
-      onClick={(e) => {
-        e.preventDefault();
-        props.onChange(city);
-      }}
-    ></a>;
-  });
+  const [cities, setCities] = useState([]);
+  const [items, setItems] = useState([]);
+  const [sortingCity, setSortingCity] = useState('');
+  const SendToList = (event) => {
+    console.log(event.target.value);
+    setSortingCity(event.target.value);
+  };
+  const refreshItems = async () => {
+    const response = await getWasteList();
+    setItems(response);
+    const result = response.map((a) => a.city);
+    setCities([...new Set(result)]);
+  };
 
+  useEffect(() => {
+    refreshItems();
+  }, []);
+  const filteredItems = items.filter((item) => {
+    if (sortingCity === '') {
+      return items;
+    }
+    return item.city === sortingCity;
+  });
   return (
-    <VersionPageHolder>
-      {
-        <Waste
-          onChange={(city) => {
-            setSelectedCity(city);
-          }}
-        />
-      }
-      <div>{renderElements(selectedCity)}</div>
-    </VersionPageHolder>
+    <div>
+      <Info>Test</Info>
+    </div>
   );
 }
 
