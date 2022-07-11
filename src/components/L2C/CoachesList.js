c55e92724079ed3d512d0fe579f293633ae6bb24
Adam Miller <theadammiller56@gmail.com>
2022-07-11 10:28:06 -0500

    Completed Unstylized CoachList

--- src/components/L2C/CoachesList.js
+++ src/components/L2C/CoachesList.js
@@ -44,19 +44,19 @@ function stableSort(array, comparator) {
 
 const headCells = [
   {
-    id: 'lastName',
+    id: 'coachLastName',
     numeric: false,
     disablePadding: false,
     label: 'Name',
   },
   {
-    id: 'email',
+    id: 'coachEmail',
     numeric: false,
     disablePadding: false,
     label: 'Email',
   },
   {
-    id: 'phoneNumber',
+    id: 'coachPhoneNumber',
     numeric: false,
     disablePadding: false,
     label: 'Phone Number',
--- src/components/L2C/L2C.js
+++ src/components/L2C/L2C.js
@@ -1,47 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import CoachesList from './CoachesList';
 
 export default function Lead2Change() {
-  const listofCoaches = [
-    {
-      id: 'c1',
-      coachFirstName: 'John',
-      coachLastName: 'Doe',
-      coachPhoneNumber: '262-555-1234',
-      coachEmail: 'John@does.org',
-      students: [
-        {
-          id: 's1',
-          studentFirstName: 'Jane',
-          studentLastName: 'Roe',
-          studentPhoneNumber: 'Jane@roes.org',
-          studentEmail: '262-555-0123',
-        },
-      ],
-    },
-    {
-      id: 'c2',
-      coachFirstName: 'Mrs',
-      coachLastName: 'Foe',
-      coachPhoneNumber: '262-555-1235',
-      coachEmail: 'f@does.org',
-      students: [
-        {
-          id: 's2',
-          studentFirstName: 'Dane',
-          studentLastName: 'Roe',
-          studentPhoneNumber: 'Dane@roes.org',
-          studentEmail: '262-555-0123',
-        },
-      ],
-    },
-  ];
+  const [coaches, setCoaches] = useState([]);
+  const refreshCoaches = async () => {
+    // TODO: API Integration
+    const listOfCoaches = [
+      {
+        id: 'c1',
+        coachFirstName: 'John',
+        coachLastName: 'Doe',
+        coachPhoneNumber: '262-555-1234',
+        coachEmail: 'John@does.org',
+        students: [
+          {
+            id: 's1',
+            studentFirstName: 'Jane',
+            studentLastName: 'Roe',
+            studentPhoneNumber: 'Jane@roes.org',
+            studentEmail: '262-555-0123',
+          },
+        ],
+      },
+      {
+        id: 'c2',
+        coachFirstName: 'Mrs',
+        coachLastName: 'Foe',
+        coachPhoneNumber: '262-555-1235',
+        coachEmail: 'f@does.org',
+        students: [
+          {
+            id: 's2',
+            studentFirstName: 'Dane',
+            studentLastName: 'Roe',
+            studentPhoneNumber: 'Dane@roes.org',
+            studentEmail: '262-555-0123',
+          },
+        ],
+      },
+    ];
+    setCoaches(listOfCoaches);
+  };
+
+  useEffect(() => {
+    refreshCoaches();
+  }, []);
+
   return (
     <div>
       <Grid container justifyContent="center" spacing={2}>
         <Grid item xs={8}>
-          <CoachesList rows={listofCoaches} />
+          <CoachesList rows={coaches} />
         </Grid>
       </Grid>
     </div>
