d8f203096905a4395a37964a7acba6797080adfa
Marv <23shahm@elmbrookstudents.org>
2022-07-13 14:10:33 -0500

    Formatting Deactivate Modal

--- src/components/L2C/InactivationModal.js
+++ src/components/L2C/InactivationModal.js
@@ -5,7 +5,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Modal from '@mui/material/Modal';
+import './InactivationModal.css';
 
 export default function AlertDialog() {
   const [open, setOpen] = React.useState(false);
@@ -24,36 +24,37 @@ export default function AlertDialog() {
 
   return (
     <div>
+      <style>.button</style>
       <Button variant="outlined" onClick={handleClickOpen}>
         Deactivate
       </Button>
-      <Modal>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle align="center" id="alert-dialog-title">
-            Are you sure you want to deactivate this coach?
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText
-              align="center"
-              color="red"
-              id="alert-dialog-description"
-            >
-              This will remove all assigned students.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleAgree} autoFocus>
-              Deactivate
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Modal>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle align="center" id="alert-dialog-title">
+          Are you sure you want to deactivate this coach?
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText
+            align="center"
+            color="red"
+            id="alert-dialog-description"
+          >
+            This will remove all assigned students.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button className="buttons-deactivate" onClick={handleAgree}>
+            Deactivate
+          </Button>
+          <Button className="buttons-cancel" onClick={handleClose}>
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
